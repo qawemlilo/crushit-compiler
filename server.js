2523f599b092c904f0f5a59e641fdcd153cd7eb8
Qawelesizwe Mlilo <qawemlilo@gmail.com>
2014-02-21 15:44:53 +0200

    added the cluster module

--- server.js
+++ server.js
@@ -2,10 +2,17 @@
     Dependancies
 */
 
-var connect = require('connect'),
+var cluster = require('cluster'),
+    connect = require('connect'),
     crushIt = require('crushit'), 
     http = require('http'), 
     app;
+    
+    
+// Spawn numCPUs worker processes
+var numCPUs = require('os').cpus().length,
+    clusterStartTime = Date.now(),
+    newWorkerEnv = {};
 
 
 app = connect()
@@ -16,18 +23,11 @@ app = connect()
 
 var port = process.env.PORT || 8080;
 
-http.createServer(app).listen(port, function() {
-  console.log('Running on port %s', port);
-});
-
-
-process.on('uncaughtException', function(err) {
-  console.log('Caught exception: ' + err);
-});
-
 
 
 function crush (req, res) {
+    "use strict";
+    
     var url = req.body.url,
         crusher = new crushIt(),    
         beautify  = !!req.body.beautify,
@@ -67,7 +67,57 @@ function crush (req, res) {
 
 
 function isURL(str) {
+    "use strict";
+    
     var urlReg = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
     
     return str.length < 2083 && urlReg.test(str);
 }
+
+
+// Master spawns worker processes
+if (cluster.isMaster) {
+    newWorkerEnv.clusterStartTime = clusterStartTime;
+    
+    // Fork workers
+    for (var i = 0; i < numCPUs; i++) {
+       console.log('Starting worker ' + i);
+       cluster.fork(newWorkerEnv);
+    }
+    
+    // If any worker process dies, spawn a new one to bring back number of processes 
+    // to be back to numCPUs
+    cluster.on('exit', function(worker, code, signal) {
+       "use strict";
+       
+       console.log('worker ' + worker.process.pid + ' died');
+       cluster.fork(newWorkerEnv);
+    });
+    
+    // Logs to know what workers are active
+    cluster.on('online', function (worker) {
+       "use strict";
+       
+       console.log('Worker ' + worker.process.pid + ' online');
+    });
+
+} else {
+
+    if (process.env.clusterStartTime) {
+        process.clusterStartTime = new Date(parseInt(process.env.clusterStartTime,10));
+    }
+
+    http.createServer(app).listen(port, function() {
+        "use strict";
+        console.log('Running on port %s', port);
+    });    
+    
+    // Graceful shutdown
+    process.on('uncaughtException', function (err) {
+        "use strict";
+        
+        console.error(err.message);
+        process.exit();
+    });
+} // end worker
+
